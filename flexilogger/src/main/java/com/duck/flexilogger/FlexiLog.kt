a68fcf0edc5079400e16d58bc94e3e3f1d2aa105
Bradley <projectdelta6@gmail.com>
2020-07-08 14:12:08 +0100

    - Added w and wtf log methods - Added dokka to generate javadocs

--- flexilogger/src/main/java/com/duck/flexilogger/FlexiLog.kt
+++ flexilogger/src/main/java/com/duck/flexilogger/FlexiLog.kt
@@ -1,7 +1,5 @@
 package com.duck.flexilogger
 
-import androidx.annotation.IntDef
-
 /**
  * Created by Bradley Duck on 2019/02/14.
  * <p/>
@@ -72,7 +70,7 @@ abstract class FlexiLog {
      */
     @JvmOverloads
     fun i(tag: String, msg: String? = null, tr: Throwable? = null) {
-        doTheStuff(i, tag, msg ?: "", tr)
+        doTheStuff(LogType.I, tag, msg ?: "", tr)
     }
 
     /**
@@ -118,7 +116,7 @@ abstract class FlexiLog {
      */
     @JvmOverloads
     fun d(tag: String, msg: String? = null, tr: Throwable? = null) {
-        doTheStuff(d, tag, msg ?: "", tr)
+        doTheStuff(LogType.D, tag, msg ?: "", tr)
     }
 
     /**
@@ -164,7 +162,7 @@ abstract class FlexiLog {
      */
     @JvmOverloads
     fun v(tag: String, msg: String? = null, tr: Throwable? = null) {
-        doTheStuff(v, tag, msg ?: "", tr)
+        doTheStuff(LogType.V, tag, msg ?: "", tr)
     }
 
     /**
@@ -192,12 +190,7 @@ abstract class FlexiLog {
      * @param tr     [Throwable] to be attached to the Log.
      */
     @JvmOverloads
-    fun e(
-        caller: Class<*>,
-        msg: String? = null,
-        tr: Throwable? = null,
-        forceReport: Boolean = false
-    ) {
+    fun e(caller: Class<*>, msg: String? = null, tr: Throwable? = null, forceReport: Boolean = false) {
         val message: String = msg ?: ""
         if (tr == null) {
             e(getClassName(caller), message, forceReport = forceReport)
@@ -215,10 +208,102 @@ abstract class FlexiLog {
      */
     @JvmOverloads
     fun e(tag: String, msg: String? = null, tr: Throwable? = null, forceReport: Boolean = false) {
-        doTheStuff(e, tag, msg ?: "", tr, forceReport)
+        doTheStuff(LogType.E, tag, msg ?: "", tr, forceReport)
     }
 
-    private fun doTheStuff(@LogType type: Int, tag: String, msg: String, tr: Throwable? = null, forceReport: Boolean = false) {
+    /**
+     * Calls [w].
+     *
+     * @param caller [Any] The caller of this method. This will be used to get the Class name of the Object.
+     * @param msg    [String] The Message to be Logged.
+     * @param tr     [Throwable] to be attached to the Log.
+     */
+    @JvmOverloads
+    fun w(caller: Any, msg: String? = null, tr: Throwable? = null) {
+        val message: String = msg ?: ""
+        if (tr == null) {
+            w(getClassName(caller), message)
+        } else {
+            w(getClassName(caller), message, tr)
+        }
+    }
+
+    /**
+     * Calls [w].
+     *
+     * @param caller [Class] The Class of the caller of this method. This will be used to get the Class name of the Object.
+     * @param msg    [String] The Message to be Logged.
+     * @param tr     [Throwable] to be attached to the Log.
+     */
+    @JvmOverloads
+    fun w(caller: Class<*>, msg: String? = null, tr: Throwable? = null) {
+        val message: String = msg ?: ""
+        if (tr == null) {
+            w(getClassName(caller), message)
+        } else {
+            w(getClassName(caller), message, tr)
+        }
+    }
+
+    /**
+     * Implement the actual Logging.
+     *
+     * @param tag [Class] The Log tag
+     * @param msg [String] The Log message.
+     * @param tr  [Throwable] to be attached to the Log.
+     */
+    @JvmOverloads
+    fun w(tag: String, msg: String? = null, tr: Throwable? = null) {
+        doTheStuff(LogType.W, tag, msg ?: "", tr)
+    }
+
+    /**
+     * Calls [wtf].
+     *
+     * @param caller [Any] The caller of this method. This will be used to get the Class name of the Object.
+     * @param msg    [String] The Message to be Logged.
+     * @param tr     [Throwable] to be attached to the Log.
+     */
+    @JvmOverloads
+    fun wtf(caller: Any, msg: String? = null, tr: Throwable? = null) {
+        val message: String = msg ?: ""
+        if (tr == null) {
+            wtf(getClassName(caller), message)
+        } else {
+            wtf(getClassName(caller), message, tr)
+        }
+    }
+
+    /**
+     * Calls [wtf].
+     *
+     * @param caller [Class] The Class of the caller of this method. This will be used to get the Class name of the Object.
+     * @param msg    [String] The Message to be Logged.
+     * @param tr     [Throwable] to be attached to the Log.
+     */
+    @JvmOverloads
+    fun wtf(caller: Class<*>, msg: String? = null, tr: Throwable? = null) {
+        val message: String = msg ?: ""
+        if (tr == null) {
+            wtf(getClassName(caller), message)
+        } else {
+            wtf(getClassName(caller), message, tr)
+        }
+    }
+
+    /**
+     * Implement the actual Logging.
+     *
+     * @param tag [Class] The Log tag
+     * @param msg [String] The Log message.
+     * @param tr  [Throwable] to be attached to the Log.
+     */
+    @JvmOverloads
+    fun wtf(tag: String, msg: String? = null, tr: Throwable? = null) {
+        doTheStuff(LogType.WTF, tag, msg ?: "", tr)
+    }
+
+    private fun doTheStuff(type: LogType, tag: String, msg: String, tr: Throwable? = null, forceReport: Boolean = false) {
         logToConsole(type, tag, msg, tr)
         reportInternal(type, tag, msg, tr, forceReport)
         writeToFileInternal(type, tag, msg, tr)
@@ -232,44 +317,32 @@ abstract class FlexiLog {
      * @param msg [String] The Log message.
      * @param tr  [Throwable] to be attached to the Log.
      */
-    private fun logToConsole(@LogType type: Int, tag: String, msg: String, tr: Throwable? = null) {
-        if (canLogToConsole(d)) {
+    private fun logToConsole(type: LogType, tag: String, msg: String, tr: Throwable? = null) {
+        if (canLogToConsole(LogType.D)) {
             if (tr != null) {
                 when (type) {
-                    i -> {
-                        android.util.Log.i(tag, msg, tr)
-                    }
-                    d -> {
-                        android.util.Log.d(tag, msg, tr)
-                    }
-                    v -> {
-                        android.util.Log.v(tag, msg, tr)
-                    }
-                    e -> {
-                        android.util.Log.e(tag, msg, tr)
-                    }
+                    LogType.I -> android.util.Log.i(tag, msg, tr)
+                    LogType.D -> android.util.Log.d(tag, msg, tr)
+                    LogType.V -> android.util.Log.v(tag, msg, tr)
+                    LogType.E -> android.util.Log.e(tag, msg, tr)
+                    LogType.W -> android.util.Log.w(tag, msg, tr)
+                    LogType.WTF -> android.util.Log.wtf(tag, msg, tr)
                 }
             } else {
                 when (type) {
-                    i -> {
-                        android.util.Log.i(tag, msg)
-                    }
-                    d -> {
-                        android.util.Log.d(tag, msg)
-                    }
-                    v -> {
-                        android.util.Log.v(tag, msg)
-                    }
-                    e -> {
-                        android.util.Log.e(tag, msg)
-                    }
+                    LogType.I -> android.util.Log.i(tag, msg)
+                    LogType.D -> android.util.Log.d(tag, msg)
+                    LogType.V -> android.util.Log.v(tag, msg)
+                    LogType.E -> android.util.Log.e(tag, msg)
+                    LogType.W -> android.util.Log.w(tag, msg)
+                    LogType.WTF -> android.util.Log.wtf(tag, msg)
                 }
             }
         }
     }
 
-    private fun reportInternal(@LogType type: Int, tag: String, msg: String, tr: Throwable? = null, forceReport: Boolean = false) {
-        if (forceReport || mustReport(e)) {
+    private fun reportInternal(type: LogType, tag: String, msg: String, tr: Throwable? = null, forceReport: Boolean = false) {
+        if (forceReport || mustReport(type)) {
             if (tr == null) {
                 report(type, tag, msg)
             } else {
@@ -278,7 +351,7 @@ abstract class FlexiLog {
         }
     }
 
-    private fun writeToFileInternal(@LogType type: Int, tag: String, msg: String, tr: Throwable? = null) {
+    private fun writeToFileInternal(type: LogType, tag: String, msg: String, tr: Throwable? = null) {
         if(mustLogToFile(type)) {
             writeLogToFile(type, tag, msg, tr)
         }
@@ -291,7 +364,7 @@ abstract class FlexiLog {
      * @param tag [Class] The Log tag
      * @param msg [String] The Log message.
      */
-    abstract fun report(@LogType type: Int, tag: String, msg: String)
+    abstract fun report(type: LogType, tag: String, msg: String)
 
     /**
      * Implement the actual reporting.
@@ -301,52 +374,29 @@ abstract class FlexiLog {
      * @param msg [String] The Log message.
      * @param tr  [Throwable] to be attached to the Log.
      */
-    abstract fun report(@LogType type: Int, tag: String, msg: String, tr: Throwable)
-
-    /**
-     * Identifier for the type of Log message.
-     * can be on of: [Companion.i], [Companion.d], [Companion.v], [Companion.e]
-     */
-    @IntDef(this.i, this.d, this.v, this.e)
-    annotation class LogType
+    abstract fun report(type: LogType, tag: String, msg: String, tr: Throwable)
 
     /**
      * Used to determine if we should Lod to the console or not.
      */
-    abstract fun canLogToConsole(@LogType type: Int): Boolean
+    abstract fun canLogToConsole(type: LogType): Boolean
 
     /**
      * Used to determine if we should send a report (to Crashlytics or equivalent)
      */
-    abstract fun mustReport(@LogType type: Int): Boolean
+    abstract fun mustReport(type: LogType): Boolean
 
     /**
      * Used to determine if we should call [writeLogToFile]
      */
-    abstract fun mustLogToFile(@LogType type: Int): Boolean
+    abstract fun mustLogToFile(type: LogType): Boolean
 
     /**
      * Implement writing of the Log to your file.
      */
-    abstract fun writeLogToFile(@LogType type: Int, tag: String, msg: String, tr: Throwable?)
+    abstract fun writeLogToFile(type: LogType, tag: String, msg: String, tr: Throwable?)
 
     companion object {
-        private val CLASS = "class: "
-        /**
-         * ID for [android.util.Log.i]
-         */
-        const val i: Int = 1
-        /**
-         * ID for [android.util.Log.d]
-         */
-        const val d: Int = 2
-        /**
-         * ID for [android.util.Log.v]
-         */
-        const val v: Int = 3
-        /**
-         * ID for [android.util.Log.e]
-         */
-        const val e: Int = 4
+        private const val CLASS = "class: "
     }
 }
