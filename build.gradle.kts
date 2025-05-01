0acf1676786cf3056bed0932b678d47d02cea9f6
Bradley <projectdelta6@gmail.com>
2025-05-01 13:28:15 +0100

    - unincorporated FlexiHttpLogger module

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    `maven-publish`
 }
 
 tasks.wrapper {
